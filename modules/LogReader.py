5f2ebfd3d2782a076faf83f14682243f5fbd215d
athulSandeep <53999163+athulSandeep@users.noreply.github.com>
2022-02-10 14:28:43 +0530

    Update LogReader.py

--- modules/LogReader.py
+++ modules/LogReader.py
@@ -7,5 +7,4 @@ m = re.search('https://cf-hls-media.sndcdn.com/playlist/(.+?)"},', data)
 if m:
     found = m.group(1)
 url = ("https://cf-hls-media.sndcdn.com/playlist/" + found)
-
 urllib.request.urlretrieve(url, r"C:\Users\hp\Desktop\misc\SE Project\Temp\playlist.m3u")
