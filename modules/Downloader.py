213fd137f1f80f52fdfb26f6bd5dd5d99408fd28
athulSandeep <53999163+athulSandeep@users.noreply.github.com>
2022-02-10 14:28:20 +0530

    path update

--- modules/Downloader.py
+++ modules/Downloader.py
@@ -6,6 +6,7 @@ with open(r"C:\Users\hp\Desktop\misc\SE Project\Temp\playlist.m3u") as file:
     for line in file:
         if (line.startswith("http")):
             links.append(line.strip("\n"))
+            
 print(links)
 with open(r"C:\Users\hp\Desktop\misc\SE Project\Downloads\Audio.mp3", 'wb') as f:
     for segment_uri in links:
