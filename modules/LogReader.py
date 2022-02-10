d01280b25518f361eb54dd7c6db45888f72c7f0c
athulSandeep <athulsandeep2@gmail.com>
2022-02-10 14:44:27 +0530

    Updated the files

--- modules/LogReader.py
+++ modules/LogReader.py
@@ -2,11 +2,11 @@ import urllib.request
 
 import regex as re
 
-with open(r'C:\Users\hp\Desktop\misc\SE Project\FantasicFour\Temp\log.txt', 'r') as file:
+with open(r'C:\Users\hp\Desktop\misc\SE Project\FantasticFour\Temp\log.txt', 'r') as file:
     data = file.read().replace('\n', '')
 m = re.search('https://cf-hls-media.sndcdn.com/playlist/(.+?)"},', data)
 if m:
     found = m.group(1)
 url = ("https://cf-hls-media.sndcdn.com/playlist/" + found)
 
-urllib.request.urlretrieve(url, r"C:\Users\hp\Desktop\misc\SE Project\FantasicFour\Temp\playlist.m3u")
+urllib.request.urlretrieve(url, r"C:\Users\hp\Desktop\misc\SE Project\FantasticFour\Temp\playlist.m3u")
